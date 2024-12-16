f489fd75cd43928eb84cbc3b5622ef13f8f7c011
Vladislav Popov <99014125+StudentVlad5@users.noreply.github.com>
2024-12-16 11:10:11 +0100

    Revert "Feat/lawyer request"

--- components/ItemPage.jsx
+++ components/ItemPage.jsx
@@ -1,39 +1,24 @@
-import Link from "next/link";
+import Link from 'next/link';
 
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 
-import { getRightData } from "../helpers/rightData";
-import { useRouter } from "next/router";
-import Image from "next/image";
+import { getRightData } from '../helpers/rightData';
+import { useRouter } from 'next/router';
+import Image from 'next/image';
 
-import styles from "../styles/itemPage.module.scss";
-import { ButtonUp } from "./ButtonUp";
-import { useTranslation } from "next-i18next";
+import styles from '../styles/itemPage.module.scss'
+import { ButtonUp } from './ButtonUp';
+import { useTranslation } from 'next-i18next';
 
-import { QRCode } from "react-qrcode-logo";
-import { OrderForm } from "./OrderForm";
-import { useState } from "react";
+import { QRCode } from 'react-qrcode-logo';
+
 
 export const ItemPage = ({ item, buttonName, linkPath }) => {
   const { locale } = useRouter();
   const { t } = useTranslation();
-  const [isActiveForm, setIsActiveForm] = useState(false);
 
-  const handleOpenForm = () => {
-    setIsActiveForm((prevState) => !prevState);
-  };
-
-  const isCalc =
-    item.id === "735806203922" ||
-    item.idPost === "Uvpy4KwwbWAO9b9QF6iX" ||
-    item.path === "monitorynh-bezvizovyi- kalkuliator";
-  const isCalcInfoArr = [
-    "services.helpConnect",
-    "services.helpControl",
-    "services.countDays",
-    "services.monitoringDocument",
-    "services.monitoringData",
-  ];
+  const isCalc = item.id === "735806203922" || item.idPost === "Uvpy4KwwbWAO9b9QF6iX" || item.path === "monitorynh-bezvizovyi- kalkuliator";
+  const isCalcInfoArr = ['services.helpConnect', 'services.helpControl', 'services.countDays', 'services.monitoringDocument', 'services.monitoringData']
 
   return (
     <div className={styles.itemPage}>
@@ -51,22 +36,12 @@ export const ItemPage = ({ item, buttonName, linkPath }) => {
 
       <div className={styles.itemPage__body}>
         <h1 className={`page__title ${styles.itemPage__title}`}>
-          {item.type === "services"
-            ? `${item.serviceType[locale]}: ${getRightData(
-                item,
-                locale,
-                "title"
-              )}`
-            : getRightData(item, locale, "title")}
+          {item.type === "services" ? `${item.serviceType[locale]}: ${getRightData(item, locale, 'title')}` : getRightData(item, locale, 'title')}
         </h1>
-        {!isCalc && (
-          <article
-            className={styles.itemPage__text}
-            dangerouslySetInnerHTML={{
-              __html: getRightData(item, locale, "text"),
-            }}
-          />
-        )}
+        {!isCalc && <article
+          className={styles.itemPage__text}
+          dangerouslySetInnerHTML={{ __html: getRightData(item, locale, 'text') }}
+        />}
         {isCalc && (
           <>
             <div className={styles.itemPage__text}>
@@ -77,36 +52,16 @@ export const ItemPage = ({ item, buttonName, linkPath }) => {
               </ul>
             </div>
             <div className={styles.itemPage__iconsWrap}>
-              <a href="https://t.me/emigrant_helper_bot" alt="">
+              <a href='https://t.me/emigrant_helper_bot' alt=''>
                 <QRCode
-                  value="https://t.me/emigrant_helper_bot"
-                  logoImage="../telegram-icon.svg"
+                  value='https://t.me/emigrant_helper_bot'
+                  logoImage='../telegram-icon.svg'
                   size={200}
                 />
               </a>
             </div>
           </>
         )}
-
-        <div className={styles.buttonDiv}>
-          <button
-            type="button"
-            onClick={handleOpenForm}
-            className={`${styles.buttonDiv__button} ${
-              isActiveForm ? styles.buttonDiv__button_active : ""
-            }`}
-          >
-            {isActiveForm ? (
-              <span className={styles.buttonDiv__button_text}>Закрити</span>
-            ) : (
-              <span className={styles.buttonDiv__button_text}>
-                Замовити послугу
-              </span>
-            )}
-          </button>
-          {isActiveForm && <OrderForm currentLanguage={locale} />}
-        </div>
-
         <button className="button-extension button-extension--down">
           <Link href={linkPath}>
             <p>{buttonName}</p>
